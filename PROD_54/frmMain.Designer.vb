cd68fd7885cfd1844af8a526265f36485dbae9c6
Chris Buckingham <66245988+cbuckingham100@users.noreply.github.com>
2021-08-11 07:49:54 +0100

    Add files via upload

---
+++ PROD_54/frmMain.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblRecords = New System.Windows.Forms.Label()
+        Me.txtOrderNum = New System.Windows.Forms.TextBox()
+        Me.lblOrderNum = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ShowCellErrors = False
+        Me.DataGridView1.ShowCellToolTips = False
+        Me.DataGridView1.ShowEditingIcon = False
+        Me.DataGridView1.ShowRowErrors = False
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 363)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(728, 416)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(60, 22)
+        Me.btnExit.TabIndex = 26
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblRecords
+        '
+        Me.lblRecords.AutoSize = True
+        Me.lblRecords.Location = New System.Drawing.Point(620, 422)
+        Me.lblRecords.Name = "lblRecords"
+        Me.lblRecords.Size = New System.Drawing.Size(47, 13)
+        Me.lblRecords.TabIndex = 31
+        Me.lblRecords.Text = "Records"
+        '
+        'txtOrderNum
+        '
+        Me.txtOrderNum.Location = New System.Drawing.Point(115, 393)
+        Me.txtOrderNum.Name = "txtOrderNum"
+        Me.txtOrderNum.Size = New System.Drawing.Size(247, 20)
+        Me.txtOrderNum.TabIndex = 32
+        '
+        'lblOrderNum
+        '
+        Me.lblOrderNum.AutoSize = True
+        Me.lblOrderNum.Location = New System.Drawing.Point(12, 396)
+        Me.lblOrderNum.Name = "lblOrderNum"
+        Me.lblOrderNum.Size = New System.Drawing.Size(73, 13)
+        Me.lblOrderNum.TabIndex = 33
+        Me.lblOrderNum.Text = "Order Number"
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblOrderNum)
+        Me.Controls.Add(Me.txtOrderNum)
+        Me.Controls.Add(Me.lblRecords)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "mainForm"
+        Me.Text = "PROD054 - Spares Cell TIJ Cartridge"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblRecords As Label
+    Friend WithEvents txtOrderNum As TextBox
+    Friend WithEvents lblOrderNum As Label
+End Class
---
+++ PROD_54/frmMain.vb
@@ -0,0 +1,207 @@
+﻿
+Imports System.Configuration
+Imports LinxLib.DataLib
+
+
+Public Class mainForm
+
+    Public sExeVersion As String = "1.00"
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+
+
+
+
+    Private Sub LoadMainFormDisplay(sOrderNum As String)
+        Dim tDatafetch(0) As DataIn
+        Dim bResult As Boolean
+        bResult = False
+
+        Dim xDataAccess As New DataAccess
+
+
+        Try
+
+            xDataAccess.Initialise()
+
+            xDataAccess.SetScheme = "dbo"
+            xDataAccess.SetTableName = "vw_oracle_tij_label_data"
+            xDataAccess.SetOrder = "unique_id"
+            If (Trim(sOrderNum) = "") Then
+                xDataAccess.SetDBWhere = "1=1"
+            Else
+                xDataAccess.SetDBWhere = "unique_id like '%" & Trim(sOrderNum) & "%'"
+            End If
+
+
+            ReDim Preserve tDatafetch(9)
+            tDatafetch(0).ColName = "Unique_ID"
+            tDatafetch(1).ColName = "Due_Date"
+            tDatafetch(2).ColName = "Product"
+            tDatafetch(3).ColName = "Qty"
+            tDatafetch(4).ColName = "Product_description"
+            tDatafetch(5).ColName = "Customer"
+            tDatafetch(6).ColName = "Cust_Code"
+            tDatafetch(7).ColName = "Country"
+            tDatafetch(8).ColName = "Cust_Order_No"
+            tDatafetch(9).ColName = "Country_Label"
+
+
+            DataGridView1.DataSource = xDataAccess.GetRecordsDataByID(tDatafetch)
+
+            lblRecords.Text = "Records: " & DataGridView1.Rows.Count
+
+        Catch
+
+
+
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub mainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        LoadMainFormDisplay("")
+
+
+    End Sub
+
+
+
+    Private Sub mainForm_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+
+        If (Me.Height < 400) Then Me.Height = 400
+        If (Me.Width < 670) Then Me.Width = 670
+        Me.DataGridView1.Width = Me.Width - 40
+        Me.DataGridView1.Height = Me.Height - 100
+        Me.btnExit.Top = Me.Height - 65
+        Me.btnExit.Left = Me.Width - 80
+        Me.lblOrderNum.Top = Me.btnExit.Top
+        Me.txtOrderNum.Top = Me.btnExit.Top
+
+        '       Me.btnCreateSK.Top = Me.btnExit.Top
+        '       Me.lblDistributor.Top = Me.btnExit.Top
+        Me.lblRecords.Top = Me.btnExit.Top
+        '        Me.lblEmail.Top = Me.btnExit.Top
+
+    End Sub
+
+    Private Sub chkActive_CheckedChanged(sender As Object, e As EventArgs)
+
+        'If chkActive.Checked Then
+        '    LoadMainFormDisplay("Y")
+        'Else
+        '    LoadMainFormDisplay("")
+        'End If
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        'Try
+        '    lblDistributor.Text = Trim(DataGridView1.Rows(e.RowIndex).Cells(5).Value.ToString)
+        '    lblEmail.Text = Trim(DataGridView1.Rows(e.RowIndex).Cells(4).Value.ToString)
+        'Catch
+
+        'End Try
+
+
+    End Sub
+
+    Private Sub btnCreateSK_Click(sender As Object, e As EventArgs)
+
+        'If Trim(lblDistributor.Text) = "" Then
+        '    MsgBox("No email for user")
+        '    Exit Sub
+        'End If
+
+        'If lblDistributor.Text = "Distributor" Then
+        '    MsgBox("Please click on distributor in the list above")
+        '    Exit Sub
+        'End If
+
+        'MsgBox("Create service key routine for " & lblDistributor.Text)
+
+        'CreateTextFile(lblDistributor.Text)
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub CreateTextFile(sDistributor As String)
+
+
+        'Dim tDatafetch(0) As DataIn
+        'Dim bResult As Boolean
+        'bResult = False
+
+        'Dim xDataAccess As New DataAccess
+
+        'Dim sServiceKeyFreq As String = ""
+        'Dim sNoOfKeys As String = ""
+
+        'Try
+
+        '    xDataAccess.Initialise()
+
+        '    xDataAccess.SetScheme = "dbo"
+        '    xDataAccess.SetTableName = "Users"
+        '    xDataAccess.SetOrder = ""
+        '    xDataAccess.SetDBWhere = "Distributor='" & sDistributor & "'"
+
+        '    ReDim Preserve tDatafetch(2)
+        '    tDatafetch(0).ColName = "Distributor"
+        '    tDatafetch(1).ColName = "ServiceKeyFreq"
+        '    tDatafetch(2).ColName = "NoOfKeys"
+
+        '    Dim oBindingSource As BindingSource
+        '    oBindingSource = xDataAccess.GetRecordsDataByID(tDatafetch)
+
+        '    sServiceKeyFreq = CType(oBindingSource.List(0), DataRowView).Item(1).ToString
+        '    sNoOfKeys = CType(oBindingSource.List(0), DataRowView).Item(2).ToString
+
+
+
+        'Catch
+
+
+
+        'End Try
+
+
+        'xDataAccess.Dispose()
+
+        'Dim sSKFileName As String = ConfigurationManager.AppSettings("ServicePasswordFileName")
+        'Dim sSKPath As String = ConfigurationManager.AppSettings("ServicePasswordPath")
+
+        'If System.IO.File.Exists(sSKPath & "\" & sSKFileName) Then
+        '    System.IO.File.Delete(sSKPath & "\" & sSKFileName)
+        'End If
+
+        'Dim sTemp As String = sDistributor & "," & sServiceKeyFreq & "," & sNoOfKeys
+        'IO.File.WriteAllText(sSKPath & "\" & sSKFileName, sTemp)
+
+
+    End Sub
+
+    Private Sub txtOrderNum_TextChanged(sender As Object, e As EventArgs) Handles txtOrderNum.TextChanged
+
+        LoadMainFormDisplay(txtOrderNum.Text)
+
+
+    End Sub
+End Class
